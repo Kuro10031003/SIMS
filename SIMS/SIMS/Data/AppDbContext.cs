44216749f5d21485d6baca4f18caf88b98b6e03c
Kuro10031003 <damionsim2005@gmail.com>
2026-05-20 16:31:30 +0800

    Add database, login system, RBAC for 3 roles

---
+++ SIMS/SIMS/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SIMS.Models;
+
+namespace SIMS.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<User>().ToTable("Users");   // maps to the Users table in SQL
+        }
+    }
+}
