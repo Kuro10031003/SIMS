7380dcab5d17774954110911d84aefe6a3bf34fa
Kuro10031003 <damionsim2005@gmail.com>
2026-06-05 13:52:08 +0800

    Add user management (list + enable/disable), complete Admin module

---
+++ SIMS/SIMS/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SIMS.Data;
+
+namespace SIMS.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        private readonly AppDbContext _db;
+        public UsersController(AppDbContext db) { _db = db; }
+
+        // 列出所有用户
+        public IActionResult Index()
+        {
+            var users = _db.Users.ToList();
+            return View(users);
+        }
+
+        // 切换激活/禁用状态
+        public IActionResult ToggleActive(int id)
+        {
+            var user = _db.Users.Find(id);
+            if (user != null)
+            {
+                user.IsActive = !user.IsActive;   // 反转：激活变禁用，禁用变激活
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
