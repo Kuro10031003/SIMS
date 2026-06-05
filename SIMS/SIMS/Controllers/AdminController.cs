2d02de29adcdce6b37fda36f76aac25e69c4f225
Kuro10031003 <damionsim2005@gmail.com>
2026-06-05 13:19:35 +0800

    Add Programme CRUD (Create, Read, Update, Delete) + Bootstrap UI + Admin dashboard

--- SIMS/SIMS/Controllers/AdminController.cs
+++ SIMS/SIMS/Controllers/AdminController.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SIMS.Data;
 
 namespace SIMS.Controllers
 {
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
-        public IActionResult Index() => View();
+        private readonly AppDbContext _db;
+        public AdminController(AppDbContext db) { _db = db; }
+
+        public IActionResult Index()
+        {
+            ViewBag.TotalStudents = _db.Students.Count();
+            ViewBag.TotalLecturers = _db.Lecturers.Count();
+            ViewBag.TotalCourses = _db.Courses.Count();
+            ViewBag.TotalProgrammes = _db.Programmes.Count();
+            return View();
+        }
     }
 }
---
+++ SIMS/SIMS/Controllers/ProgrammesController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SIMS.Data;
+using SIMS.Models;
+
+namespace SIMS.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class ProgrammesController : Controller
+    {
+        private readonly AppDbContext _db;
+        public ProgrammesController(AppDbContext db) { _db = db; }
+
+        public IActionResult Index()
+        {
+            var programmes = _db.Programmes.ToList();
+            return View(programmes);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Programme programme)
+        {
+            _db.Programmes.Add(programme);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var programme = _db.Programmes.Find(id);
+            if (programme == null) return NotFound();
+            return View(programme);
+        }
+
+        // EDIT - 处理修改提交（POST）
+        [HttpPost]
+        public IActionResult Edit(Programme programme)
+        {
+            _db.Programmes.Update(programme);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // DELETE - 显示确认页（GET）
+        public IActionResult Delete(int id)
+        {
+            var programme = _db.Programmes.Find(id);
+            if (programme == null) return NotFound();
+            return View(programme);
+        }
+
+        // DELETE - 确认删除（POST）
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var programme = _db.Programmes.Find(id);
+            if (programme != null)
+            {
+                _db.Programmes.Remove(programme);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
